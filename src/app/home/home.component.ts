bff42ac7f026384505eee7d8c2b12fa0a7d6bf26
Alexey <algoritm211@gmail.com>
2022-10-19 23:36:58 +0300

    feat: add displaying courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,39 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
+import {Course} from '../model/course';
 import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {catchError, delayWhen, filter, map, pluck, retryWhen, share, shareReplay, tap} from 'rxjs/operators';
+import {createHttpObservable} from '../common/util';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
+  beginnerCourses$: Observable<Course[]>;
+  advancesCourses$: Observable<Course[]>;
 
-    constructor() {
+  constructor() {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
 
+    const courses$ = http$.pipe(
+      pluck('payload'),
+      share()
+    ) as Observable<Course[]>;
 
+    this.beginnerCourses$ = courses$.pipe(
+      map(val => val.filter(val => val.category === 'BEGINNER'))
+    );
 
-    }
+    this.advancesCourses$ = courses$.pipe(
+      map(val => val.filter(val => val.category === 'ADVANCED'))
+    );
+  }
 
 }
